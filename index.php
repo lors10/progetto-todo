177146760bd14b35bf29844cf99c66cab057f00f
Lorenzo Salvi <lorenzosalvi@MacBook-Air-di-Lorenzo.local>
2022-04-27 19:09:39 +0200

    prove di codice visto a lezione

--- index.php
+++ index.php
@@ -1,51 +1,13 @@
-<!DOCTYPE html>
+<?php
 
-<html>
+    require "include/template.inc.php";
 
-<head>
-    <title>Todo</title>
+    $main = new Template("skins/index.html");
+    $header = new Template("skins/header.html");
+    $footer = new Template("skins/footer.html");
 
-    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    $main->setContent("header",$header->get());
+    $main->setContent("footer",$footer->get());
+    $main->close();
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div id="container">
-
-    <!-- Form -->
-    <div>
-
-        <h1 id="title-start-message">Todo helps peaple or Teams to stay focused on their works!</h1>
-        <p id="text-start-message">
-            Collaborate, manage projects, and reach new productivity peaks.
-            From high rises to the home office,
-            the way your team works is unique—accomplish it all with Todo.
-        </p>
-
-    </div>
-
-    <form method="post" action="#">
-        <div id="button-container">
-
-            <div>
-                <button type="submit" class="button" id="button-login"><a class="link-button" href="identifications/signup.php"> Sign up </a></button>
-            </div>
-
-            <br>
-
-            <div>
-                <button type="submit" class="button"><a class="link-button" href="identifications/login.php"> Log in </a></button>
-            </div>
-
-        </div>
-    </form>
-
-
-</div>
-
-</body>
-
-</html>
+?>
